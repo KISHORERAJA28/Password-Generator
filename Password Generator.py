05c80ee903e122797a3cca6f96a37dc6087590a0
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-03-04 23:08:11 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,7 +1,7 @@
 import string
 import secrets
 
-234567890123456789
+23456789012345678
 def create_secure_password(length=16):
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
